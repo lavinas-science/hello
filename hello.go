cb1f77e5e04b49ddd8a9711d2ea4a28f6679ae85
lavinas-science <lavinas@gmail.com>
2021-08-31 19:03:33 -0300

    Change 2

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/lavinas-science/hello/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
